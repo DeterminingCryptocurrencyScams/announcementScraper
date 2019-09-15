fde75acbe07e05e63db0f10caaa3474b89acf8c2
atotalnoob <atotalnoob@users.noreply.github.com>
2019-09-14 17:29:54 -0700

    Added phase 1 codes

---
+++ scraper/AnnoucementScraper/AnnoucementScraper/Program.cs
@@ -0,0 +1,14 @@
+﻿using AnnoucementScraper.core.crawler;
+using System;
+
+namespace AnnoucementScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program is starting");
+            new AnnTaskScraper().Scrape();
+        }
+    }
+}
