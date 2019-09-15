3fdbf43192554b77e1673f390decb6953307cf68
atotalnoob <atotalnoob@users.noreply.github.com>
2019-09-14 17:32:20 -0700

    Revert "Revert "Added phase 1 codes""
    This reverts commit 05d7593ca0f26d09e8536bf52d80e6104aebee3d.

---
+++ scraper/AnnoucementScraper/AnnoucementScraper/core/crawler/Xpath/AnnCollectorXpaths.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnnoucementScraper.core.crawler.Xpaths
+{
+    public class AnnCollectorXpaths
+    {
+        public static string BaseSelector { get; set; } = "/html[1]/body[1]/div[2]/div[2]/table[1]";
+        public static string FirstPageBaseSelector { get; set; } = "/html[1]/body[1]/div[2]/div[3]/table[1]";
+    }
+}
